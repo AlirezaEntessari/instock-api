632610d8234518a36a996bbd50eca2af2c007e0e
Andry <andrewcozy852@gmail.com>
2023-11-09 16:18:26 -0500

    i make the api put and edit for inventory items

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const knex = require('knex')(require('../knexfile'));
 
 router.get('/', async (req, res) => {
-	const data = await knex('inventories')
-		.join('warehouses', 'warehouses.id', 'inventories.warehouse_id')
-		.select('inventories.id', 'warehouses.warehouse_name', 'item_name', 'description', 'category', 'status', 'quantity');
-	res.json(data);
+	try {
+		const data = await knex('inventories')
+			.join('warehouses', 'warehouses.id', 'inventories.warehouse_id')
+			.select('inventories.id', 'warehouses.warehouse_name', 'item_name', 'description', 'category', 'status', 'quantity');
+		res.json(data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
 router.post('/seed', async (req, res) => {
@@ -16,6 +21,71 @@ router.post('/seed', async (req, res) => {
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	const inventoryId = req.params.id;
+	const {
+		warehouse_id,
+		item_name,
+		description,
+		category,
+		status,
+		quantity,
+	} = req.body;
+
+	if (
+		!warehouse_id ||
+		!item_name ||
+		!description ||
+		!category ||
+		!status ||
+		!quantity
+	) {
+		return res.status(400).json({ error: 'All fields are required.' });
+	}
+
+	if (isNaN(quantity)) {
+		return res.status(400).json({ error: 'Quantity must be a number.' });
+	}
+
+	try {
+		const existingInventory = await knex('inventories')
+			.where({ id: inventoryId })
+			.first();
+
+		if (!existingInventory) {
+			return res.status(404).json({ error: 'Inventory item not found.' });
+		}
+
+		const existingWarehouse = await knex('warehouses')
+			.where({ id: warehouse_id })
+			.first();
+
+		if (!existingWarehouse) {
+			return res.status(400).json({ error: 'Invalid warehouse_id.' });
+		}
+
+		await knex('inventories')
+			.where({ id: inventoryId })
+			.update({
+				warehouse_id,
+				item_name,
+				description,
+				category,
+				status,
+				quantity,
+			});
+
+		const updatedInventory = await knex('inventories')
+			.where({ id: inventoryId })
+			.first();
+
+		return res.status(200).json(updatedInventory);
+	} catch (error) {
+		console.error('Error updating inventory item:', error);
+		return res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
 
