a3cf59132d5b5ca2776865e69bef6a9f66095fe9
AZubaidi <abdulrahman.zubaidi@gmail.com>
2023-11-09 12:31:45 -0500

    fix seeds, add get all inventories

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
+
+router.get('/', async (req, res) => {
+	const data = await knex('inventories');
+	console.log(data);
+	res.json(data);
+})
 
 module.exports = router;
