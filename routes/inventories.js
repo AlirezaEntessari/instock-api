5c878379efe913db2cf84eb9114b219d24c0320f
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-11 18:45:25 -0500

    create delete router

--- routes/inventories.js
+++ routes/inventories.js
@@ -88,5 +88,25 @@ router.put('/:id', async (req, res) => {
 		return res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
+router.delete('/:id', async (req, res) => {
+    const warehouseId = req.params.id;
 
+    try {
+        const existingWarehouse = await knex('warehouses').where({ id: warehouseId }).first();
+
+        if (!existingWarehouse) {
+            return res.status(404).json({ error: 'Warehouse not found' });
+        }
+
+        await knex.transaction(async (trx) => {
+            await trx('inventory').where({ warehouse_id: warehouseId }).del();
+            await trx('warehouses').where({ id: warehouseId }).del();
+        });
+
+        return res.status(204).send();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 module.exports = router;
